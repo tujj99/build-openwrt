9325553774279b712119783ebc864fcba12b65d2
tujj99 <53027340+tujj99@users.noreply.github.com>
2024-01-09 22:07:01 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -8,8 +8,8 @@
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
-sed -i '$a src-git kenzok8 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/tujj99/small.git' feeds.conf.default
+sed -i '$a src-git tujj99 https://github.com/tujj99/openwrt-packages' feeds.conf.default
 
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
