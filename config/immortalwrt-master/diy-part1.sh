1e2c376e888decd5bfd6f694e39916591d8169e7
tujj99 <53027340+tujj99@users.noreply.github.com>
2024-03-24 20:20:06 +0800

    Update diy-part1.sh add kenzok8 packages

--- config/immortalwrt-master/diy-part1.sh
+++ config/immortalwrt-master/diy-part1.sh
@@ -9,6 +9,16 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
+# Add for passwall2 package compile by tujj99 20240324
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
+
+
 # other
 # rm -rf package/emortal/{autosamba,ipv6-helper}
 
